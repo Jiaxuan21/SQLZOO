729ba2f95884ad0bd780f9191675e425e93eeec9
Jiaxuan21 <36943258+Jiaxuan21@users.noreply.github.com>
2018-03-02 15:55:54 -0500

    Update Join.sql

--- Join.sql
+++ Join.sql
@@ -4,9 +4,8 @@ a shorter way of saying matchid, teamid, player, gtime
 
 Modify it to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
 */
-```SQL
+
 SELECT matchid, player FROM goal 
   WHERE teamid = 'GER'
-```
   
 /* 2.

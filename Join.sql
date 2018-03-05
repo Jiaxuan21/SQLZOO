91b9585ab3c4b4eb01016b3dd3a5c0ffa8465e89
Jiaxuan21 <36943258+Jiaxuan21@users.noreply.github.com>
2018-03-05 10:48:33 -0500

    Update Join.sql
    Answer for The JOIN operation.
    https://sqlzoo.net/wiki/The_JOIN_operation

--- Join.sql
+++ Join.sql
@@ -100,3 +100,33 @@ group by stadium
 /* 11.
 For every match involving 'POL', show the matchid, date and the number of goals scored
 */
+SELECT matchid,mdate,count(*)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL')
+ group by matchid,mdate
+ 
+/* 12.
+For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
+*/
+select matchid,mdate,count(*)
+from game join goal on matchid = id
+where teamid = 'GER'
+group by matchid, mdate
+
+/* 13.
+List every match with the goals scored by each team as shown. This will use "CASE WHEN" which has not been explained in any previous exercises.
+mdate	team1	score1	team2	score2
+1 July 2012	ESP	4	ITA	0
+10 June 2012	ESP	1	ITA	1
+10 June 2012	IRL	1	CRO	3
+...
+Notice in the query given every goal is listed. If it was a team1 goal then a 1 appears in score1, otherwise there is a 0. You could SUM this column to get a count of the goals scored by team1. Sort your result by mdate, matchid, team1 and team2. 
+*/
+SELECT mdate,
+  team1,
+  sum(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) as score1,
+  team2,
+  sum(case when teamid=team2 then 1 else 0 end) as score2
+  FROM game left JOIN goal ON id = matchid
+group by mdate,team1,team2
+order by mdate,matchid,team1,team2

9a37b4484c39d9c978d6f8efc9f1e8f2d8989444
Jiaxuan21 <36943258+Jiaxuan21@users.noreply.github.com>
2018-03-06 17:00:57 -0500

    Update Self_Join.sql

--- Self_Join.sql
+++ Self_Join.sql
@@ -96,4 +96,15 @@ from route a join route b on
   join stops s3 on c.stop=s3.id
 where s1.name = 'Craiglockhart'
 and s3.name = 'Sighthill'
-
+?????????????
+SELECT DISTINCT a.num, a.company, stopb.name ,  c.num,  c.company
+FROM route a JOIN route b
+ON (a.company = b.company AND a.num = b.num)
+JOIN ( route c JOIN route d ON (c.company = d.company AND c.num= d.num))
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+JOIN stops stopc ON (c.stop = stopc.id)
+JOIN stops stopd ON (d.stop = stopd.id)
+WHERE  stopa.name = 'Craiglockhart' AND stopd.name = 'Sighthill'
+            AND  stopb.name = stopc.name
+ORDER BY LENGTH(a.num), b.num, stopb.id, LENGTH(c.num), d.num
